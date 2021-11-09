004e93fa871c8d20faab93be441de56307ea5e5c
alaJream <jeremy.gonzales@students.dominican.edu>
2021-11-08 16:35:33 -0800

    finished TODO, cleaned up code

--- hero.py
+++ hero.py
@@ -5,89 +5,50 @@ from armor import Armor
 from weapon import Weapon
 
 class Hero:
-  # We want our hero to have a default "starting_health",
-  # so we can set that in the function header.
   def __init__(self, name, starting_health=100):
-    '''Instance properties:
-        abilities: List
-        armors: List
-        name: String
-        starting_health: Integer
-        current_health: Integer
-    '''
-    # abilities and armors don't have starting values,
-    # and are set to empty lists on initialization
     self.abilities = list()
     self.armors = list()
-    # we know the name of our hero, so we assign it here
     self.name = name
-    # similarly, our starting health is passed in, just like name
     self.starting_health = starting_health
-    # when a hero is created, their current health is
-    # always the same as their starting health (no damage taken yet!)
     self.current_health = starting_health
     self.deaths = 0
     self.kills = 0
 
   def add_ability(self, ability):
-    ''' Add ability to abilities list '''
-
-    # We use the append method to add ability objects to our list.
     self.abilities.append(ability)
-  def attack(self):
-    '''Calculate the total damage from all ability attacks.
-        return: total_damage:Int
-    '''
 
-    # start our total out at 0
+  def add_weapon(self, weapon):
+    self.abilities.append(weapon)
+
+  def attack(self):
     total_damage = 0
-    # loop through all of our hero's abilities
     for ability in self.abilities:
-      # add the damage of each attack to our running total
       total_damage += ability.attack()
-    # return the total damage
     return total_damage
+
   def add_armor(self, armor):
-    '''Add armor to self.armors
-        Armor: Armor Object
-    '''
-    # TODO: Add armor object that is passed in to `self.armors`
     self.armors.append(armor)
+
   def defend(self):
-    '''Calculate the total block amount from all armor blocks.
-        return: total_block:Int
-    '''
-    # TODO: This method should run the block method on each armor in self.armors
+    total_block = 0
+    for armor in self.armors:
+        total_block += armor.block()
+    return total_block 
+
   def take_damage(self, damage):
-    '''Updates self.current_health to reflect the damage minus the defense.
-    '''
-    # TODO: Create a method that updates self.current_health to the current
-    # minus the the amount returned from calling self.defend(damage).
-    damage -= self.defend()
-    if damage < 0:
-        damage = 0
-  def is_alive(self):  
-    '''Return True or False depending on whether the hero is alive or not.
-    '''
-    # TODO: Check the current_health of the hero.
-    # if it is <= 0, then return False. Otherwise, they still have health
-    # and are therefore alive, so return True
+    defense = self.defend()
+    if defense < damage:
+        self.current_health -= (damage - defense)
     return self.current_health
-  def fight(self, opponent):  
-    ''' Current Hero will take turns fighting the opponent hero passed in.
-    ~~ will also return True if the fight is won or false if the fight is lost ~~
-    '''
-    # TODO: Fight each hero until a victor emerges.
-    # Phases to implement:
-    # 0) check if at least one hero has abilities. If no hero has abilities, print "Draw"
+
+  def is_alive(self):  
+    return self.current_health >= 0
+
+  def fight(self, opponent): 
     if len(self.abilities) <= 0 and len(opponent.abilities) <= 0:
         print("Draw")
         return
-    # 1) else, start the fighting loop until a hero has won
     while True:
-        # 2) the hero (self) and their opponent must attack each other and each must take damage from the other's attack
-        # 3) After each attack, check if either the hero (self) or the opponent is alive   
-        # 4) if one of them has died, print "HeroName won!" replacing HeroName with the name of the hero, and end the fight loop
         self.take_damage(opponent.attack())
         print(self.is_alive())
         print(self.current_health)
@@ -96,7 +57,7 @@ class Hero:
             opponent.add_kill(1)
             self.add_death(1)
             return False
-        
+    
         opponent.take_damage(self.attack())
         if opponent.is_alive() == False:
             print(f"{self.name} won!")
@@ -104,33 +65,16 @@ class Hero:
             opponent.add_death(1)
             return True
 
-    # TODO: Refactor this method to update the following:
-    # 1) the number of kills the hero (self) has when the opponent dies.
-    # 2) then number of kills the opponent has when the hero (self) dies
-    # 3) the number of deaths of the opponent if they die    in the fight
-    # 4) the number of deaths of the hero (self) if they die in the fight
-
-  def add_weapon(self, weapon):
-    '''Add weapon to self.abilities'''
-    # TODO: This method will append the weapon object passed in as an
-    # argument to self.abilities.
-    # This means that self.abilities will be a list of
-    # abilities and weapons.
   def add_kill(self, num_kills):
-    ''' Update self.kills by num_kills amount'''
     self.kills += num_kills
+
   def add_death(self, num_deaths):
-    ''' Update deaths with num_deaths'''
-    # TODO: This method should add the number of deaths to self.deaths
-    pass
+    self.deaths += num_deaths
 
 
 
 
 if __name__ == "__main__":
-    # If you run this file from the terminal
-    # this block is executed.
-
     hero1 = Hero("Wonder Woman")
     hero2 = Hero("Dumbledore")
     ability1 = Ability("Super Speed", 300)
--- weapon.py
+++ weapon.py
@@ -3,10 +3,6 @@ from random import randint
 
 class Weapon(Ability):
   def attack(self):
-    """  This method returns a random value
-    between one half to the full attack power of the weapon.
-    """
-    # TODO: Use integer division to find half of the max_damage value
-    # then return a random integer between half of max_damage and max_damage
-    pass
+    attack_dmg = randint(int(self.max_damage)//2,int(self.max_damage))
+    return attack_dmg
 
